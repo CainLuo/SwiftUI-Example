39ec62c133f70c55037d01a596cf7adaec3d05e0
CainLuo <350116542@qq.com>
2024-01-14 19:57:37 +0800

    Add LottieAnimations Example

---
+++ 17.LottieAnimations/LottieAnimations/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LottieAnimations
+//
+//  Created by Cain Luo on 2024/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 17.LottieAnimations/LottieAnimations/LottieAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsApp.swift
+//  LottieAnimations
+//
+//  Created by Cain Luo on 2024/1/14.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
