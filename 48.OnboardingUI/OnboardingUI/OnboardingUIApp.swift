b6c57c82c9a7f027a91ed975d2f3357718879e44
CainLuo <350116542@qq.com>
2024-01-27 16:03:00 +0800

    Add OnboardingUI Example

---
+++ 48.OnboardingUI/OnboardingUI/OnboardingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingUIApp.swift
+//  OnboardingUI
+//
+//  Created by Cain Luo on 2024/1/26.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
